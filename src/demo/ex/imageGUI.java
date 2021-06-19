f3b28c46f7aabf6cc3c0d3a24c063fce0f79fc8c
Justina0331 <83816503+Justina0331@users.noreply.github.com>
2021-06-19 21:57:08 +0800

    Update imageGUI.java

--- src/demo/ex/imageGUI.java
+++ src/demo/ex/imageGUI.java
@@ -277,41 +277,7 @@ public class imageGUI {
             super.insertString(offset, str, attrSet);
         }
     }
-    
-    //判斷使用者有沒有input，未輸入給予預設值
-    class CustomFocusListener implements FocusListener {
-    	
-    	private String hintText;
-    	private JTextField textField;
-    	public CustomFocusListener(JTextField jTextField,String hintText) {
-    		this.textField = jTextField;
-    		this.hintText = hintText;
-    		jTextField.setText(hintText);  //默認提示內容
-    		jTextField.setForeground(Color.GRAY);
-    	}
-     
-    	@Override
-    	public void focusGained(FocusEvent e) {
-    		//使用者若有輸入，清空提示内容
-    		String temp = textField.getText();
-    		if(temp.equals(hintText)) {
-    			textField.setText("");
-    			textField.setForeground(Color.BLACK);
-    		}	
-    	}
-     
-    	@Override
-    	public void focusLost(FocusEvent e) {	
-    		//失去輸入內容時，顯示提示內容
-    		String temp = textField.getText();
-    		if(temp.equals("")) {
-    			textField.setForeground(Color.GRAY);
-    			textField.setText(hintText);
-    		}
-    		
-    	}
 
-    }
     private void checkInput(ActionEvent e2) {
     	
     	//將text字串轉為數字
